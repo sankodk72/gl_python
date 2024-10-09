919fe45432fbf8b3851bda3aeb08ca2c216f524e
Oleksandr Kovalenko <san.kovalenko@gmail.com>
2024-10-09 18:50:27 +0300

    creation repo and preparing to the first task

---
+++ task2/main.py
@@ -0,0 +1,7 @@
+'''Implement python script that calculate visible color of star based on its
+temperature. The colors can be blue, white, yellow, orange and red.
+Input of the script should be dictionary where key is name of the star
+and value is a temperature. Output of the script should be colors
+separated by newlines that has at least one star in it spectrum and star
+names separated by comma. Script should has at least two functions:
+one for calculating values and one for printing it.'''
