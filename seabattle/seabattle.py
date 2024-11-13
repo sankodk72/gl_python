dcba801d11b6cdfda9e5603834677d156f620cd6
Oleksandr Kovalenko <san.kovalenko@gmail.com>
2024-11-13 17:48:32 +0200

    squads & players

--- seabattle/seabattle.py
+++ seabattle/seabattle.py
@@ -1,38 +1,66 @@
-import random
-
 class Ship:
-    def __init__(self, power, armour):
+    def __init__(self, name, power, armour):
+        self.name = name
         self.power = power
         self.armour = armour
+    
+    def info(self):
+        return f"Name: {self.name}, Power: {self.power}, Armour: {self.armour}"
 
-class Squad(Ship):
+class Squad:
     def __init__(self):
-        super.__init__()
+#        super().__init__(power, armour)
+        self.ships = []
 
-    aircraft = Ship(5,5)
-    combat = Ship(4,4)
-    submarine1 = Ship(3,3)
-    submarine2 = Ship(3,3)
-    patrol1 = Ship(3,3)
-    patrol2 = Ship(3,3)
-    usv1 = Ship(1,1)
-    usv2 = Ship(1,1)
-    usv3 = Ship(1,1)
-    usv4 = Ship(1,1)
+    def add_ship(self, name, power, armour):
+        ship = Ship(name, power, armour)
+        self.ships.append(ship)
 
-    Squadlist = [aircraft, combat, submarine1, submarine2, patrol1,
-                  patrol2, usv1, usv2, usv3, usv4]
+    def show_squad_info(self):
+        for i, ship in enumerate(self.ships, 1):
+            print(f"{ship.info()}")
 
-Squad1 = Squad
-print(Squad1.Squadlist) 
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.squad = Squad()
 
+    def show_player_squad_info(self):
+        print (f"{self.name}'s squad:")
+        self.squad.show_squad_info()
 
-class Player(Squad):
-    def __init__(self, player1, player2):
-        super.__init__()
-        self.player1 = player1
-        self.player2 = player2
+print()
 
-    def hit(self):
-        self.armour.player2 = self.armour.player2 - (self.power.player1*random(0.2 - 1))
-        self.armour.player1 = self.armour.player1 - (self.power.player2*random(0.2 - 1))
+Player1 = Player("Player1")
+
+Player1.squad.add_ship("aircraft11", 5, 5)  
+Player1.squad.add_ship("combat11", 4, 4)  
+Player1.squad.add_ship("submarine11", 3, 3)
+Player1.squad.add_ship("submarine12", 3, 3)
+Player1.squad.add_ship("patrol11", 2, 2)
+Player1.squad.add_ship("patrol12", 2, 2)
+Player1.squad.add_ship("usv11", 1, 1)
+Player1.squad.add_ship("usv12", 1, 1)
+Player1.squad.add_ship("usv13", 1, 1)
+Player1.squad.add_ship("usv14", 1, 1) 
+
+Player1.show_player_squad_info()
+
+print()
+
+Player2 = Player("Player2")
+
+Player2.squad.add_ship("aircraft21", 5, 5)  
+Player2.squad.add_ship("combat21", 4, 4)  
+Player2.squad.add_ship("submarine21", 3, 3)
+Player2.squad.add_ship("submarine22", 3, 3)
+Player2.squad.add_ship("patrol21", 2, 2)
+Player2.squad.add_ship("patrol22", 2, 2)
+Player2.squad.add_ship("usv21", 1, 1)
+Player2.squad.add_ship("usv22", 1, 1)
+Player2.squad.add_ship("usv23", 1, 1)
+Player2.squad.add_ship("usv24", 1, 1) 
+
+Player2.show_player_squad_info()
+
+print()
